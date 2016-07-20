51556cf25faee668ed5b8e8f1ea77c30d357ecac
daoducnha <daoducnha29493@gmail.com>
2016-07-20 13:20:54 +0700

    Bai 1. Hello

---
+++ Bai1DaoDucNha/src/Bai1_DaoDucNha.java
@@ -0,0 +1,13 @@
+
+public class Bai1_DaoDucNha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("**  **  ******  **      **       ****");
+		System.out.println("**  **  **      **      **      **  **");
+		System.out.println("******  ******  **      **      **  **");
+		System.out.println("**  **  **      **      **      **  **");
+		System.out.println("**  **  ******  ******  ******   ****");
+	}
+
+}
